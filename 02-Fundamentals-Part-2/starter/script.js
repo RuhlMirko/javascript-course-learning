70c80a1dd5bd922f4594c444a8036ac2527a5295
Mirko <ruhlmirkojoel@gmail.com>
2024-11-19 15:23:31 -0300

    Added functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -8,3 +8,17 @@ if (hasDriversLicense) console.log("I can drive");
 
 //const interface = 'Audio'
 //const private = 534;
+
+function logger(name = "Joel") {
+  console.log(`Hello ${name}`);
+}
+
+logger("Mirko");
+logger();
+
+function sum_of_nums(a, b) {
+  const result = `The result of ${a} + ${b} is ${a + b}`;
+  return result;
+}
+
+console.log(sum_of_nums(5, 8));
