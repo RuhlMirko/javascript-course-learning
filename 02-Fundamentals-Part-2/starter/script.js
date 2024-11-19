587b98c639740443897b2f98fdf9eb8475bfcb99
Mirko <ruhlmirkojoel@gmail.com>
2024-11-19 15:31:42 -0300

    Added more examples

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,10 +9,10 @@ if (hasDriversLicense) console.log("I can drive");
 //const interface = 'Audio'
 //const private = 534;
 
+// Functions
 function logger(name = "Joel") {
   console.log(`Hello ${name}`);
 }
-
 logger("Mirko");
 logger();
 
@@ -20,5 +20,19 @@ function sum_of_nums(a, b) {
   const result = `The result of ${a} + ${b} is ${a + b}`;
   return result;
 }
-
 console.log(sum_of_nums(5, 8));
+
+const age1 = calcAge1(1991);
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+// Arrow functions
+
+const age3 = (birthYear) => 2037 - birthYear;
+
+console.log(age1);
