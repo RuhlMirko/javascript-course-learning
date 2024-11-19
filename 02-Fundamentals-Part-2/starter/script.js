9e8dbd2b025c19bdaa17ac6803bc814d88f9b52c
Mirko <ruhlmirkojoel@gmail.com>
2024-11-19 15:34:51 -0300

    Added arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -31,8 +31,15 @@ const calcAge2 = function (birthYear) {
   return 2037 - birthYear;
 };
 const age2 = calcAge2(1991);
+
 // Arrow functions
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
 
-const age3 = (birthYear) => 2037 - birthYear;
-
-console.log(age1);
+const yearsUntilRetirement = (birthYear) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return retirement;
+};
+console.log(yearsUntilRetirement(1991));
